517377f2e2b290446b19f07437b8e945fa3484d0
oa1cspace <oa1cspace@gmail.com>
2020-09-24 15:40:08 +0300

    &&&7777

--- src/container/components/messageListComponent.jsx
+++ src/container/components/messageListComponent.jsx
@@ -1,37 +1,85 @@
 import React, {useEffect, useState} from "react";
 import connect from "react-redux/lib/connect/connect";
-import {useDispatch, useSelector} from "react-redux";
+import socket from "../../socket";
+
+// socket connection with a server;
+socket.on('connect', () => {
+});
+
+function MessageListComponent({}) {
+
+    const [message, setMessage] = useState({text: '', id: '', name: ''});
+    const [chat, setChat] = useState([]);
+
+    useEffect(() => {
+        socket.on('add_message', handleMessage);                   // on 'event' - listen  <== server
+    }, [chat])   // write here dependencies from handlers
+
+    const handleMessage = (messData) => {
+        console.log('===[ messData ]=====>', messData);
+        setChat([...chat, {...messData}])
+    };
+
+    const onTextChange = e => {
+        console.log('ON TEXT CHANGE',)
+        setMessage({...message, [e.target.name]: e.target.value});
+        console.log('===[ message ]=====>', message);
+    };
+
+    const onMessageSubmit = () => {
+        const {name, id, text} = message
+        console.log('===[ onMessageSubmit ]=====>', message);
+        socket.emit('message', {name, id, text})                      // emit 'event' - send to  ==> server
+        setMessage({text: '', id: '', name})
+    };
 
 
-function MessageListComponent({activeChat, chat}) {
 
-    const [message, setMessage] = useState('');
-    // const dispatch = useDispatch()
-    // const messages = useSelector((store) => store.messages)
-    const messages = [{message: 'Hello', id: 1, name: 'Anna'} , {message: 'Hi!', id: 2, name: "Andre"}];
+    // const renderChat = () => {
+    //     if (!chat.length) {
+    //         return null;
+    //     }
+    //     return chat.map(({name, text}, index) => (
+    //         <div key={index}>
+    //             {/*<h3>*/}
+    //             {/*    {name}: <span>{message}</span>*/}
+    //             {/*</h3>*/}
+    //
+    //             <ul>
+    //                 <li className="replies">
+    //                     <img src="https://rozetked.me/images/uploads/dwoilp3BVjlE.jpg" alt=""/>
+    //                     <p className="p"><span className="name-block">{index.name}:</span>
+    //                         <span className="messages-span"> </span> {index.text}
+    //                     </p>
+    //                 </li>
+    //             </ul>
+    //
+    //         </div>
+    //     ))
+    // };
 
-    const handleChange = (event) => {
-        const {target} = event;
-        setMessage( target.value
-        )
-        console.log("message", message)
-    }
 
-    const handleSubmit = () => {
-        const newMessage = messages.push({
-            message: message,
-            id: 1,
-            name: 'Garry'
-            //match.params.user.name
-            //match.params.user.id
-        })
+    // const [message, setMessage] = useState('');
+    // const messages = [{message: 'Hello', id: 1, name: 'Anna'} , {message: 'Hi!', id: 2, name: "Andre"}];
 
-        setMessage('')
-
-        console.log("newMessage", newMessage)
-        console.log("messages", messages)
-
-    }
+    // const handleChange = (event) => {
+    //     const {target} = event;
+    //     setMessage( target.value
+    //     )
+    //     console.log("[=== message =====>]", message);
+    // }
+    // const handleSubmit = () => {
+    //     const newMessage = messages.push({
+    //         message: message,
+    //         id: 1,
+    //         name: 'Garry'
+    //         //match.params.user.name
+    //         //match.params.user.id
+    //     })
+    //     setMessage('')
+    //     console.log("newMessage", newMessage);
+    //     console.log("messages", messages);
+    // }
 
     return (
         <div className="content">
@@ -42,48 +90,40 @@ function MessageListComponent({activeChat, chat}) {
                 </p>
             </div>
             <div className="messages">
-
-                {
-                    chat.messages.map((m, ) => {
-
+                {chat.map((m, ) => {
                         return (
                             <ul >
                                 <li className="replies" >
                                     <img src="https://rozetked.me/images/uploads/dwoilp3BVjlE.jpg" alt="" />
-                                    <p className="p"> <span className="name-block">{m.name}:</span><span className="messages-span" > </span> {m.message}
+                                    <p className="p">
+                                        {/*<span className="name-block">{m.name}:</span>*/}
+                                        <span className="messages-span" > </span> {m.text}
                                     </p>
                                 </li>
                             </ul>
                         );
-                    })}
-
+                    })
+                }
             </div>
+
             <div className="message-input">
                 <div className="wrap">
                     <input
                         type="text"
-                        placeholder="Напишите сообщение..."
-                        name='message'
-                        onChange={handleChange}
+                        placeholder="Type message..."
+                        name='text'
+                        onChange={onTextChange}
                         maxLength="500"
-                        value={ message }
-
+                        value={message.text}
                     />
-                    <button onClick={handleSubmit}>
+
+                    <button onClick={onMessageSubmit}>
                         <span className="material-icons pb-2 icon-size">send</span>
                     </button>
                 </div>
             </div>
         </div>
     )
-
 }
 
-const mapStateToProps = (state) => {
-    return {
-        // chats: chatsSelector(state),
-    };
-};
-
-// export default connect(mapStateToProps, {  })(MessageListComponent);
 export default MessageListComponent;
--- src/container/loginContainer.jsx
+++ src/container/loginContainer.jsx
@@ -14,7 +14,6 @@ export const LoginContainer = ({}) => {
         dispatch(signInGoogleRequest(name));
     }
 
-
      return(
          <div>
               <form className="form-staff">
